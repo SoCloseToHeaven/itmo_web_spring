ce8cba4ec45fe3736b5e865c8486c7703235e66d
SoCloseToHeaven <halsofods@gmail.com>
2023-11-26 00:12:15 +0300

    initialize API gateway

---
+++ api-gateway/src/main/kotlin/ru/ifmo/soclosetoheaven/ApiGatewayApplication.kt
@@ -0,0 +1,13 @@
+package ru.ifmo.soclosetoheaven
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayApplication>(*args)
+}
