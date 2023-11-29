70303a49e25760c2f05e327c49b3f96a5331cc9b
SoCloseToHeaven <halsofods@gmail.com>
2023-11-29 23:12:41 +0300

    add user entity

---
+++ auth-service/src/main/kotlin/ru/ifmo/soclosetoheaven/entity/User.kt
@@ -0,0 +1,42 @@
+package ru.ifmo.soclosetoheaven.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+
+@Entity(name = "users")
+@Table(name = "users")
+data class User(
+
+    @Column(name = "username")
+    private val username: String,
+    @Column(name = "password")
+    private val password: String,
+
+) : UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+
+    // override default functions
+    override fun getPassword(): String = password
+    override fun getUsername(): String = username
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+
+}
