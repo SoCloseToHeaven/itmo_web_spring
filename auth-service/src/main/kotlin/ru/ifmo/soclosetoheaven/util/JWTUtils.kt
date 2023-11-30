acd891fe924d5744acfd5194543abb6b70753620
SoCloseToHeaven <halsofods@gmail.com>
2023-11-30 15:50:35 +0300

    add JWTSecret

--- auth-service/src/main/kotlin/ru/ifmo/soclosetoheaven/util/JWTUtils.kt
+++ auth-service/src/main/kotlin/ru/ifmo/soclosetoheaven/util/JWTUtils.kt
@@ -1,6 +1,7 @@
 package ru.ifmo.soclosetoheaven.util
 
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 
@@ -11,6 +12,14 @@ class JWTUtils {
         private const val TOKEN_HEADER = "Authorization"
         private const val TOKEN_PREFIX = "Bearer "
     }
+
+
+    @Value("\${jwt.secret}")
+    lateinit var secret: String
+
+    @Value("\${jwt.expires}")
+    private var expires: Long = 604800000 // Week in miliseconds
+
 
     fun resolveToken(req: HttpServletRequest) : String? {
         val header = req.getHeader(TOKEN_HEADER)
