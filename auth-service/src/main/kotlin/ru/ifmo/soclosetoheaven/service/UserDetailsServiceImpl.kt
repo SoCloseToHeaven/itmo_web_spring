41f750d11ba4d46b9c1a183f6c175297ef7c762f
SoCloseToHeaven <halsofods@gmail.com>
2023-12-01 00:18:20 +0300

    add userdetails service implementation class for later spring security config

---
+++ auth-service/src/main/kotlin/ru/ifmo/soclosetoheaven/service/UserDetailsServiceImpl.kt
@@ -0,0 +1,27 @@
+package ru.ifmo.soclosetoheaven.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import ru.ifmo.soclosetoheaven.repository.UserRepository
+
+
+@Service
+class UserDetailsServiceImpl : UserDetailsService {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException("No user found with name=$username")
+        return User.builder()
+            .username(user.username)
+            .password(user.password)
+            .build()
+    }
+
+}
