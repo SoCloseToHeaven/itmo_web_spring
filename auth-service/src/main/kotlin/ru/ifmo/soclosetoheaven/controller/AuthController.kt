f58c33fda45af4f6be14638ece89f063033d5ea8
SoCloseToHeaven <halsofods@gmail.com>
2023-12-01 14:42:16 +0300

    add AuthController with mappings

---
+++ auth-service/src/main/kotlin/ru/ifmo/soclosetoheaven/controller/AuthController.kt
@@ -0,0 +1,24 @@
+package ru.ifmo.soclosetoheaven.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.ifmo.soclosetoheaven.dto.UserRequest
+import ru.ifmo.soclosetoheaven.service.UserService
+
+
+@RestController
+@RequestMapping("/auth")
+class AuthController {
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @PostMapping(value = ["/login"])
+    fun login(@RequestBody userRequest: UserRequest) = userService.login(userRequest)
+
+    @PostMapping(value = ["/signup"])
+    fun signup(@RequestBody userRequest: UserRequest) = userService.signup(userRequest)
+}
