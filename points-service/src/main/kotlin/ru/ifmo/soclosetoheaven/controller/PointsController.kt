635a6d9789689fe6730d82910bea5b74694ba2f9
SoCloseToHeaven <halsofods@gmail.com>
2023-12-02 20:40:44 +0300

    add PointsController and all endpoints

---
+++ points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/controller/PointsController.kt
@@ -0,0 +1,31 @@
+package ru.ifmo.soclosetoheaven.controller
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.ifmo.soclosetoheaven.dto.JwtDTO
+import ru.ifmo.soclosetoheaven.dto.PointRequest
+import ru.ifmo.soclosetoheaven.service.PointService
+
+
+@RestController
+@RequestMapping("/points")
+class PointsController(
+    val pointService: PointService,
+    var jwtDTO: JwtDTO
+) {
+
+    @PostMapping
+    fun newPoint(@RequestBody request: PointRequest) = pointService.createNewPoint(request, jwtDTO.userId)
+
+
+    @GetMapping( "/{current_r}")
+    fun getPoints(@PathVariable("current_r") r: Double) = pointService.getAllPointsByCreatorId(jwtDTO.userId, r)
+
+    @DeleteMapping
+    fun deleteAllPoints() = pointService.deleteAllUserPoints(jwtDTO.userId)
+}
--- points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/service/PointService.kt
+++ points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/service/PointService.kt
@@ -2,6 +2,7 @@ package ru.ifmo.soclosetoheaven.service
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import ru.ifmo.soclosetoheaven.dto.PointRequest
 import ru.ifmo.soclosetoheaven.dto.PointResponse
 import ru.ifmo.soclosetoheaven.repository.PointRepository
@@ -10,25 +11,22 @@ import java.util.*
 
 
 @Service
-class PointService {
-
-    @Autowired
-    private lateinit var pointRepository: PointRepository
-
-    @Autowired
-    private lateinit var pointMapper: PointMapper
-
-
-
+class PointService(
+    private val pointRepository: PointRepository,
+    private val pointMapper: PointMapper,
+) {
+    @Transactional
     fun getAllPointsByCreatorId(creatorId: Long, currentR: Double) = pointRepository
         .findAllByCreatorId(creatorId)
         .map { entity -> pointMapper.mapToResponse(entity, currentR) }
         .toList()
 
+
+    @Transactional
     fun deleteAllUserPoints(creatorId: Long) = pointRepository.deleteAllByCreatorId(creatorId)
 
 
-
+    @Transactional
     fun createNewPoint(pointRequest: PointRequest, creatorId: Long) : PointResponse {
         val entity = pointMapper.mapToEntity(pointRequest, creatorId)
         pointRepository.save(entity)
