8783ee0fed5ca087e299635777bb5aac91a9b9db
SoCloseToHeaven <halsofods@gmail.com>
2023-12-06 11:23:41 +0300

    add authorization page

---
+++ frontend/src/pages/auth/AuthorizationPage.tsx
@@ -0,0 +1,44 @@
+import AuthorizationForm from "./components/AuthorizationForm";
+import {PersonalCredentials} from "./components/PersonalCredentials";
+import React, {useState} from "react";
+import {Alert} from "react-bootstrap";
+import {useNavigate} from 'react-router-dom';
+
+
+export default function AuthorizationPage() {
+    const [failText, setFailText] = useState<string>("");
+
+    const navigate = useNavigate();
+    const onSuccess = () => navigate("/points");
+
+    const onFail = (message: string) => setFailText(message);
+
+    return (
+        <div className="text-center">
+            <div className="mb-3 mt-3" id="credits-container">
+                <PersonalCredentials />
+            </div>
+            <div
+                className="mb-3 mt-3 w-50 rounded border border-3 shadow-lg container gap-3 text-center"
+                id="auth-container"
+            >
+                <AuthorizationForm onSuccess={onSuccess} onFail={onFail} />
+            </div>
+            <div
+                className="text-center w-25"
+            >
+                <Alert
+
+                    variant="danger"
+                    key="danger"
+                    hidden={failText.length === 0}
+                >
+                    <Alert.Heading>
+                        Unable to auth user!
+                    </Alert.Heading>
+                    {failText}
+                </Alert>
+            </div>
+        </div>
+    );
+}
