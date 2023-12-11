73a6dbebd7f8fae318fd222ceda6a877b17ee5b5
SoCloseToHeaven <halsofods@gmail.com>
2023-12-11 14:20:56 +0300

    rewrite points validation

--- points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/controller/PointsController.kt
+++ points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/controller/PointsController.kt
@@ -1,5 +1,8 @@
 package ru.ifmo.soclosetoheaven.controller
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
 import org.springframework.web.bind.annotation.*
 import ru.ifmo.soclosetoheaven.dto.JwtDTO
 import ru.ifmo.soclosetoheaven.dto.PointRequest
@@ -14,11 +17,17 @@ class PointsController(
 ) {
 
     @PostMapping
-    fun newPoint(@RequestBody request: PointRequest) = pointService.createNewPoint(request, jwtDTO.userId)
+    fun newPoint(@Valid @RequestBody request: PointRequest) = pointService.createNewPoint(request, jwtDTO.userId)
 
 
     @GetMapping( "/{current_r}")
-    fun getPoints(@PathVariable("current_r") r: Double) = pointService.getAllPointsByCreatorId(jwtDTO.userId, r)
+    fun getPoints(
+        @PathVariable("current_r")
+        @Valid
+        @DecimalMin("1.0")
+        @DecimalMax("5.0")
+        r: Double
+    ) = pointService.getAllPointsByCreatorId(jwtDTO.userId, r)
 
     @DeleteMapping
     fun deleteAllPoints() = pointService.deleteAllUserPoints(jwtDTO.userId)
--- points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/dto/PointRequest.kt
+++ points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/dto/PointRequest.kt
@@ -2,16 +2,15 @@ package ru.ifmo.soclosetoheaven.dto
 
 import jakarta.validation.constraints.DecimalMax
 import jakarta.validation.constraints.DecimalMin
-import jakarta.validation.constraints.Min
 
 data class PointRequest(
-    @DecimalMin("-7.5")
-    @DecimalMax("7.5")
+    @field:DecimalMin("-7.5")
+    @field:DecimalMax("7.5")
     val x: Double,
-    @DecimalMin("-7.5")
-    @DecimalMax("7.5")
+    @field:DecimalMin("-7.5")
+    @field:DecimalMax("7.5")
     val y: Double,
-    @DecimalMin("1.0")
-    @DecimalMax("5.0")
+    @field:DecimalMin("1.0")
+    @field:DecimalMax("5.0")
     val r: Double,
 )
