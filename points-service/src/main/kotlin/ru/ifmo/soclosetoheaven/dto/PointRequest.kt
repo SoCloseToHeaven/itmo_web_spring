7f508934694ceffb3f6c22d94023a1f06a30b3b1
SoCloseToHeaven <halsofods@gmail.com>
2023-12-11 13:32:52 +0300

    add point request validation

--- points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/dto/PointRequest.kt
+++ points-service/src/main/kotlin/ru/ifmo/soclosetoheaven/dto/PointRequest.kt
@@ -1,7 +1,17 @@
 package ru.ifmo.soclosetoheaven.dto
 
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.Min
+
 data class PointRequest(
+    @DecimalMin("-7.5")
+    @DecimalMax("7.5")
     val x: Double,
+    @DecimalMin("-7.5")
+    @DecimalMax("7.5")
     val y: Double,
+    @DecimalMin("1.0")
+    @DecimalMax("5.0")
     val r: Double,
 )
